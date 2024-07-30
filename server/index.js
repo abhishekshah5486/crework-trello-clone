8cf3a52b634dba4143b90bab64c0cd14f7ab6c8f
abhishekshah5486 <abhishek.shah5486@gmail.com>
2024-07-30 11:43:38 +0530

    Created models

--- server/index.js
+++ server/index.js
@@ -9,23 +9,8 @@ mongoose.connect(process.env.MONGODB_URI).then(() => {
     console.log(err);
 })
 
-// User Schema
-const UserSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
-    }
-})
-const UserModel = mongoose.model('users', UserSchema);
 const app = express();
+app.use(express.json());
 const port = process.env.PORT || 8086;
 
 // Define a route for the root URL
---
+++ server/models/user.js
@@ -0,0 +1,22 @@
+// User Schema
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+    },
+    password: {
+        type: String,
+        required: true
+    },
+}, {
+    timestamps: true
+})
+const UserModel = mongoose.model('users', UserSchema);
+export default UserModel;
