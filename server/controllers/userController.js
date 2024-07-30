7168bb4f525f06c5f121db31bc6be0aaef9c46d5
abhishekshah5486 <abhishek.shah5486@gmail.com>
2024-07-30 14:27:47 +0530

    Integrated axios

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -8,14 +8,13 @@ exports.getUserDetails = async (req, res) => {
 exports.registerUser = async(req, res) => {
     try {
         // Check if the email already registered
-        const emailAlreadyRegistered = userModel.findOne({email: req.body.email});
+        const emailAlreadyRegistered = await userModel.findOne({email: req.body.email});
         if (emailAlreadyRegistered){
-            res.send({
+            return res.send({
                 success: false,
                 message: "Email already exists."
             })
         }
-
         // Salting and Password Hashing
         const salt = await bcrypt.genSalt(10);
         const hashedPassword = await bcrypt.hash(req.body.password, salt);
@@ -23,11 +22,48 @@ exports.registerUser = async(req, res) => {
 
         const newUser = new userModel(req.body);
         await newUser.save();
-        res.status(201).send({
+        return res.status(201).send({
             success: true,
             message: "Registration successful."
         })
     } catch (err) {
-        res.json(err);
+        return res.status(500).json({
+            success: false,
+            message: "Server error.",
+            error: err.message
+        });
+
+    }
+}
+
+exports.loginUser = async(req, res) => {
+    try {
+        // Check if the email address is registered
+        const emailExists = await userModel.findOne({email: req.body.email});
+
+        if (!emailExists){
+            return res.send({
+                success: false,
+                message: "Email is not registered, please register."
+            })
+        }
+        const validPassword = await bcrypt.compare(req.body.password, emailExists.password);
+        if (!validPassword){
+            return res.send({
+                success: false,
+                message: "Invalid password."
+            })
+        }
+        return res.status(201).send({
+            success: true,
+            message: "Login successful."
+        })
+    } catch (err) {
+        return res.status(500).json({
+            success: false,
+            message: "Server error.",
+            error: err.message
+        });
+
     }
 }
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,12 +1,11 @@
 const express = require('express');
-const app = express();
 const router = express.Router();
 
 const userControllers = require('../controllers/userController');
-const userModel = require('../models/userModel');
 // Define a route for the root URL
 router.get('/', userControllers.getUserDetails);
 
 
 router.post('/signup', userControllers.registerUser);
+router.post('/login', userControllers.loginUser);
 module.exports = router;
